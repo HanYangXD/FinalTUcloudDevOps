334866ca39192b2a68faa56d59cb131cc96acf59
Choo Han Yang <aaronchoo3313@gmail.com>
2021-06-22 18:48:15 +0800

    after delete packages subfolders

--- ThAmCo.Auth/Services/ThAmCoDefaults.cs
+++ ThAmCo.Auth/Services/ThAmCoDefaults.cs
@@ -10,4 +10,4 @@
         //     Default value for AuthenticationScheme property in the JwtBearerAuthenticationOptions
         public const string AuthenticationScheme = "thamco_account_api";
     }
-}
+} 
